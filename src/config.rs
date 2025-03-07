930d007643183ae30f9748d8c9a1c8d1ce45652e
zzbnews <github@zzb.news>
2025-03-07 17:50:28 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.104.142.177"];
+pub const RS_PUB_KEY: &str = "8cPi4+AlAQqvZGshirHpYPBddiYWNvYdj4OwpNkxPWg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
